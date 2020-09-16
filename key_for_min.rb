ef28ebc3539698923cf3e25cc8f4221643bd8abc
Kevin Glasgow <kglas001@gmail.com>
2020-09-16 20:52:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,11 @@
 
 def key_for_min_value(name_hash)
   the_key = nil 
-  starting =
+  starting = name_hash[id][0]
   name_hash.each do |id, num|
     if num <= starting 
-      
+      the_key = id
+    end
+  end
+  the_key
 end
